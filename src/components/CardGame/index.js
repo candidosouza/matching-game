b772e4e8829709e4eaa0c193eae7c686d97c7a53
Candido Souza <candidosouzza@gmail.com>
2021-09-22 08:41:50 -0300

    :tada: Start project
    Start project

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const CardGame = () => {
+    return `
+        <article class="card-game">
+            <img src="./src/images/pixel.png" alt="pixel" />
+        </article>
+    `;
+}
+
+export default CardGame;
